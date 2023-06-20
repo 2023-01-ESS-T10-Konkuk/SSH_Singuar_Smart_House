fbf52c7fedf3421046afba65f18e202bdf311e8b
kr1124 <parkrae1124@gmail.com>
2023-06-20 21:14:01 +0900

    fan_control_complete

--- fan_con/fan_con.c
+++ fan_con/fan_con.c
@@ -10,6 +10,8 @@
 #include <linux/interrupt.h>
 #include <linux/cdev.h>
 
+#include "fan_con.h"
+
 MODULE_LICENSE("GPL");
 
 #define MAX_TIMING 85
@@ -19,11 +21,13 @@ MODULE_LICENSE("GPL");
 #define CYCLE 10
 #define CHECKING 500
 
+#define DEV_NAME "fan_con_dev"
+
 static int dht11_data[5] = {0, };
 
 int activation = 0;
 
-static void dht11_read(void) {
+static int dht11_read(void) {
 	int last_state = 1;
 	int counter = 0;
 	int i = 0, j = 0;
@@ -68,7 +72,7 @@ static void dht11_read(void) {
 	if( (j >= 40) && (dht11_data[4] == ( (dht11_data[0] + dht11_data[1] + dht11_data[2] + dht11_data[3]) & 0xFF) ) ) {
 		printk("Humidity: %d.%d Temperature = %d.%d C\n" , dht11_data[0] , dht11_data[1] , dht11_data[2] , dht11_data[3]);
 		
-		if(dht11_data[0] > 41) {				
+		if(dht11_data[0] > 40) {				
 			activation = 1;
 		}
 		else {
@@ -78,21 +82,56 @@ static void dht11_read(void) {
 	else {
 		printk("Data not good, skip\n");
 	}
+	
+	return activation;
 }
 
-static int __init fan_con_init(void) {
-	printk("fan_con: Init Module\n");
+static int fan_con_open(struct inode *inode, struct file *file) {
+	printk("fan_con: open\n");
+	return 0;
+}
 
+static int fan_con_release(struct inode *inode, struct file *file) {
+	printk("fan_con: release\n");
+	return 0;
+}
+
+static long fan_con_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+	int ret = 0;
+	
+	switch(cmd) {
+		case FAN_ACTIVE:
+			gpio_set_value(FAN, arg);
+			break;
+		case DHT11_GET:
+			ret = dht11_read();
+			return ret;
+		default:
+			return -1;
+	}
+	return 0;
+}
+
+struct file_operations fan_con_ioctl_fops = {
+	.open = fan_con_open,
+	.release = fan_con_release,
+	.unlocked_ioctl = fan_con_ioctl,
+};
+
+static dev_t dev_num;
+static struct cdev *cd_cdev;
+
+static int __init fan_con_init(void) {
 	gpio_request_one(FAN, GPIOF_OUT_INIT_LOW, "FAN");
 	gpio_request_one(DHT11, GPIOF_IN, "DHT11");
     
-    //must use another way
-	/*while(1) {
-    		dht11_read();
-		gpio_set_value(FAN, activation);
-    		mdelay(CHECKING);
-	}*/
+	printk("fan_con: Init Module\n");
 
+    alloc_chrdev_region(&dev_num, 0, 1, DEV_NAME);
+	cd_cdev = cdev_alloc();
+	cdev_init(cd_cdev, &fan_con_ioctl_fops);
+	cdev_add(cd_cdev, dev_num, 1);
+    
 	return 0;
 }
 
@@ -103,6 +142,9 @@ static void __exit fan_con_exit(void) {
 	gpio_set_value(FAN, 0);
 	gpio_free(FAN);
 	gpio_free(DHT11);
+	
+	cdev_del(cd_cdev);
+	unregister_chrdev_region(dev_num, 1);
 }
 
 module_init(fan_con_init);
